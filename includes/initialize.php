3d4f4c44ac286ec7143486aaace1265e3390e9f3
Johan Eckdahl <johaneckdahl@ucsb.edu>
2017-02-10 16:36:31 -0800

    Moved directory root definition to config.php

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,20 +1,16 @@
 <?php
 
+require_once("config.php");
+
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null :
-	define('SITE_ROOT', '/var/www/html/webpages');
-
 defined('LIB_PATH') ? null :
-	define('LIB_PATH', SITE_ROOT.'/includes');
-
-
+        define('LIB_PATH', SITE_ROOT.'/includes');
 
 require_once (LIB_PATH."/database.php");
 require_once (LIB_PATH."/functions.php");
 //require_once (LIB_PATH."/session.php");
 //require_once (LIB_PATH."/user.php");
-require_once ("config.php");
 require_once (LIB_PATH."/databaseobject.php");
 
 ?>
