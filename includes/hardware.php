7cd5f0a2c0483736896128d3d6a419c881f63a48
Johan Eckdahl <johaneckdahl@ucsb.edu>
2017-02-27 15:43:49 -0800

    Add find location to hardware.php

--- includes/hardware.php
+++ includes/hardware.php
@@ -7,7 +7,18 @@ class Hardware extends DatabaseObject {
 
 	protected static function get_extra_attributes($object){
 		parent::get_extra_attributes($object);
-		
+		global $database;
+
+		//Find location with long-winded SQL
+		$sql  = "SELECT date, recipient FROM shipment";
+		$sql .= " WHERE id IN";
+		$sql .= " (SELECT shipment_id FROM shipment_item";
+		$sql .= " WHERE table_name= '".static::$table_name."'";
+		$sql .= " AND table_key = ".$object->id;
+		$sql .= " ) ORDER BY date DESC LIMIT 1";
+		$result = $database->query($sql);
+		$object->location = ($database->fetch_assoc($result))['recipient'];
+
 	}
 
 }
