676c32e2d0d3490afbc05ebcc35f5d36f018af6c
Johan Eckdahl <johaneckdahl@ucsb.edu>
2017-05-02 15:06:48 -0700

    add labview query

--- public/labview/query.php
+++ public/labview/query.php
@@ -4,17 +4,20 @@ require_once("../../includes/initialize.php");
 
 if(isset($_GET['tray_number'])){
 	$tray_number = $_GET['tray_number'];
+
+	$objects = Module::find_by_attribute('tray_number', $tray_number );
+
+	foreach($objects as $object){
+		echo $object->id.",";
+		echo $object->position.",";
+		echo $object->thickness.",";
+		echo $object->top_layer.",";
+	}
+}else{
+	echo "needs tray number";
 }
 
-$objects = Module::find_by_attribute("location", $tray_number );
 
-foreach($objects as $object){
-	echo $object->id."\n";
-	echo $object->position."\n";
-	echo $object->thickness."\n";
-	echo $object->top_layer."\n";
-}
 
-print_r(array(1,2,3,4));
 
 ?>
